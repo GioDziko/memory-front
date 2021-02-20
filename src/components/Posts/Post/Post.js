35fe6fb70b3284b65fca73998d562e93770a2443
GioDziko <73946192+GioDziko@users.noreply.github.com>
2021-02-21 00:57:09 +0400

    First commit

---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useStyles from './styless';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { ThumbUpAltIcon } from "@material-ui/icons/ThumbUpAlt";
+
+const Post = () => {
+    const classes=useStyles();
+    return (
+        <div>
+            <h1>Post</h1>
+        </div>
+    )
+}
+
+export default Post
